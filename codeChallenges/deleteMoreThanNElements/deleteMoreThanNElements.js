b8bc0a8d6d3f4d87e91284ed585a6280c646dbe9
Kris3579 <kristianesvelt@hotmail.com>
2019-04-13 16:02:48 -0700

    make new array

--- codeChallenges/deleteMoreThanNElements/deleteMoreThanNElements.js
+++ codeChallenges/deleteMoreThanNElements/deleteMoreThanNElements.js
@@ -2,29 +2,23 @@
 
 function limitNumberOfInvidualElements(arr, n) {
   const elements = new Map([]);
+  const newArr = [];
 
   for (let i = 0; i < arr.length; i++) {
-    console.log(elements);
     if (elements[arr[i]]) {
-      console.log(`instance of ${arr[i]} exists`);
-      if (elements[arr[i]] >= n) {
-        console.log(`deleting instance of ${arr[i]} for surpassing limit`);
-        arr.splice(i, 1);
-      }
-
       if (elements[arr[i]] < n) {
-        console.log(`adding to counter for ${arr[i]}`);
         elements[arr[i]] += 1;
+        newArr.push(arr[i]);
       }
     }
 
     if (!elements[arr[i]]) {
-      console.log(`creating map value for ${arr[i]}`);
       elements[arr[i]] = 1;
+      newArr.push(arr[i]);
     }
   }
 
-  return arr;
+  return newArr;
 }
 
 module.exports = limitNumberOfInvidualElements;
